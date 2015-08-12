35ff86597a58314654beee94e2d7086d70849821
Derek Warren <warren@sfu.ca>
2015-08-12 14:58:26 -0700

    Initial attempt at Fedora 22+ cups-browsed support

--- manifests/browsed/service.pp
+++ manifests/browsed/service.pp
@@ -4,20 +4,48 @@ class cups::browsed::service {
   # Since CUPS 1.6, there is a separate daemon responsible for discovering
   # network printers using Avahi and optionally the legacy CUPS protocol.
 
-  validate_re ($::operatingsystem, 'CentOS', 'cups-browsed only exists with SystemD cups in CentOS')
-  validate_re ($::operatingsystemmajrelease, '7', 'cups-browsed only exists with SystemD cups in CentOS')
+  case $::operatingsystem {
+    CentOS: {
+      case $::operatingsystemmajrelease {
+        7: {
+          if !defined(Package["cups"]) {
+            package { 'cups':
+              ensure => present,
+            }
+          }
+          service { 'cups-browsed':
+            ensure  => running,
+            pattern => 'cups-browsed',
+            enable  => true,
+            require => Package['cups'],
+          }
+        } # end 7
+        default: {
+          fail("Unsupported platform.")
+        } # end default
+      } # end ::operatingsystemmajrelease
+    } # end CentOS
 
-  if !defined(Package["cups"]) {
-    package { 'cups':
-      ensure => present,
-    }
-  }
-
-  service { 'cups-browsed':
-    ensure  => running,
-    pattern => 'cups-browsed',
-    enable  => true,
-    require => Package['cups'],
-  }
-
+    Fedora: {
+      case $::operatingsystemmajrelease {
+        22: {
+          if !defined(Package['cups-filters']) {
+            # don't ask me why cups-browsed is provided by cups-filters
+            package { ['cups-filters']:
+              ensure => present,
+            }
+          }
+          service { 'cups-browsed':
+            ensure  => running,
+            pattern => 'cups-browsed',
+            enable  => true,
+            require => Package['cups-filters'],
+          }
+        } # end 22
+        default: {
+          fail("Unsupported platform.")
+        } # end default
+      } # end ::operatingsystemmajrelease
+    } # end Fedora
+  } # end ::operatingsystem
 }
