753cf629519d2d560757297028d05d68120f2710
Derek Warren <warren@sfu.ca>
2015-10-23 13:52:28 -0700

    Add Ubuntu support.

--- manifests/browsed/service.pp
+++ manifests/browsed/service.pp
@@ -48,5 +48,30 @@ class cups::browsed::service {
         } # end default
       } # end ::operatingsystemmajrelease
     } # end Fedora
+
+    Ubuntu: {
+      case $::operatingsystemmajrelease {
+        # facter 2.3 reports major release number as 15.04, 15.10.
+        # thanks for nothing, facter.
+
+        /^15\./: {
+          if !defined(Package['cups-browsed']) {
+            package { ['cups-browsed']:
+              ensure => present,
+            }
+          }
+          service { 'cups-browsed':
+            provider => 'systemd',
+            ensure   => running,
+            pattern  => 'cups-browsed',
+            enable   => true,
+            require  => Package['cups-browsed'],
+          }
+        } # end 15.x
+        default: {
+          fail("Unsupported platform.")
+        } # end default
+      } # end ::operatingsystemmajrelease
+    } # end Ubuntu
   } # end ::operatingsystem
 }
