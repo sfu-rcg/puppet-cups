0e6fa0d2ed99d8e62e08845c67eb2a04de5aea7a
Adam S <asa188@sfu.ca>
2015-10-26 16:01:53 -0700

    Cleaned up implementation of Fedora 22 and Ubuntu 15.04 support with browsed
    Moved data and decisions into params.pp, did linting cleanup all around and submitted warning about browsed being unsupported instead of failing.

--- manifests/browsed/service.pp
+++ manifests/browsed/service.pp
@@ -4,74 +4,31 @@ class cups::browsed::service {
   # Since CUPS 1.6, there is a separate daemon responsible for discovering
   # network printers using Avahi and optionally the legacy CUPS protocol.
 
-  case $::operatingsystem {
-    CentOS: {
-      case $::operatingsystemmajrelease {
-        7: {
-          if !defined(Package["cups"]) {
-            package { 'cups':
-              ensure => present,
-            }
-          }
-          service { 'cups-browsed':
-            ensure  => running,
-            pattern => 'cups-browsed',
-            enable  => true,
-            require => Package['cups'],
-          }
-        } # end 7
-        default: {
-          fail("Unsupported platform.")
-        } # end default
-      } # end ::operatingsystemmajrelease
-    } # end CentOS
+  # Report to the server and client that browsed from this module is unsupported on those operating systems and don't execute the class
+  if $::cups::browsed_support == true {
+    if $cups::package_cups_browsed {
+      package { 'cups-browsed':
+        name   => $cups::package_cups_browsed,
+        ensure => present,
+      }
+    }
 
-    Fedora: {
-      case $::operatingsystemmajrelease {
-        22: {
-          if !defined(Package['cups-filters']) {
-            # don't ask me why cups-browsed is provided by cups-filters
-            package { ['cups-filters']:
-              ensure => present,
-            }
-          }
-          service { 'cups-browsed':
-            provider => 'systemd',
-            ensure   => running,
-            pattern  => 'cups-browsed',
-            enable   => true,
-            require  => Package['cups-filters'],
-          }
-        } # end 22
-        default: {
-          fail("Unsupported platform.")
-        } # end default
-      } # end ::operatingsystemmajrelease
-    } # end Fedora
+    $_package_cups_browsed = $cups::package_cups_browsed ? {
+      undef   => 'cups',
+      default => 'cups-browsed',
+    }
 
-    Ubuntu: {
-      case $::operatingsystemmajrelease {
-        # facter 2.3 reports major release number as 15.04, 15.10.
-        # thanks for nothing, facter.
-
-        /^15\./: {
-          if !defined(Package['cups-browsed']) {
-            package { ['cups-browsed']:
-              ensure => present,
-            }
-          }
-          service { 'cups-browsed':
-            provider => 'systemd',
-            ensure   => running,
-            pattern  => 'cups-browsed',
-            enable   => true,
-            require  => Package['cups-browsed'],
-          }
-        } # end 15.x
-        default: {
-          fail("Unsupported platform.")
-        } # end default
-      } # end ::operatingsystemmajrelease
-    } # end Ubuntu
-  } # end ::operatingsystem
+    service { 'cups-browsed':
+      name    => $::cups::service_cups_browsed,
+      ensure  => running,
+      pattern => 'cups-browsed',
+      enable  => true,
+      require => Package[$_package_cups_browsed],
+    }
+  }
+  else {
+    $_unsup_message = "${::operatingsystem} ${::operatingsystemrelease} not supported by browsed section of the cups module"
+    notice($_unsup_message)
+    notify { $_unsup_message: withpath => true }
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -8,10 +8,40 @@ class cups::params {
   $service_ensure = 'running'
   $service_enabled = true
   $service_name = 'cups'
+  $service_cups_browsed = 'cups-browsed'
 
   $cups_lpd_enable = false
   $cups_lpd_ensure = 'running'
   $package_cups_lpd = 'cups-lpd'
   $config_file = 'puppet:///modules/cups/cups-lpd'
   $cups_browsed_enable = false
+  $package_cups_browsed = $::operatingsystem ? {
+    CentOS => undef,
+    Fedora => 'cups-filters',
+    Ubuntu => 'cups-browsed',
+  }
+  # Browsed Class verification for whether supported or not
+  case $::operatingsystem {
+    CentOS: {
+      $browsed_support = $::operatingsystemmajrelease ? {
+        7       => true,
+        default => false,
+      }
+    } # end CentOS
+    Fedora: {
+      $browsed_support = $::operatingsystemmajrelease ? {
+        22      => true, 
+        default => false,
+      }
+    } # end Fedora
+    Ubuntu: {
+      $browsed_support = $::operatingsystemmajrelease ? {
+        /^15\.\d+$/ => true,
+        default     => false,
+      }
+    } # end Ubuntu
+    default: {
+      $browsed_support = false
+    }
+  } # end ::operatingsystem
 }
