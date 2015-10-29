887048f5c9466dec2742598923b000d22f631674
asasfu <asa188@sfu.ca>
2015-10-29 03:02:31 -0700

    Upgraded documentation and added ensure
    Linting documentation

--- manifests/config/rawtemplate.pp
+++ manifests/config/rawtemplate.pp
@@ -1,11 +1,33 @@
-# Enables the user to create cupsd.conf and cups-browsed.conf files
-# with the content of their choosing
-# $file is the file you'll write to
-# $values is an array of lines you want to write to the file
-# $service is the service you want to refresh when the file gets changed, 
-#   this should be cups or cups-browsed only most likely
-# $chmod_value is for file mode, we will force 0640 if it's cupsd.conf though as that is required on that file
+# == Define: cups::config::rawtemplate
+#
+# This defined type enables the user to createcupsd.conf and cups-browsed.conf files with the
+# content of their choosing
+#
+# === Parameters
+#
+# [*ensure*]
+#   Controls presence of the file that you're templating
+#
+# [*file*]
+#   The file to write to, we only support files inside /etc/cups directory with this define
+#
+# [*values*]
+#   Array of lines which you wish to write to the file
+#
+# [*service*]
+#   The service you wish to refresh/restart when the file gets changed(most likely cups or cups-browsed)
+#   Even if cups-browsed is another name on your OS, we tend to reference the service via cups-browsed
+#
+# [*chmod_value*]
+#   File mode for the templated file, we will force 0640 if it's cupsd.conf though as that seems to be 
+#   requried on that file
+#
+# === Authors
+#
+# * Adam S <mailto:asa188@sfu.ca>
+#
 define cups::config::rawtemplate (
+  $ensure      = file,
   $file        = $name,
   $values      = [],
   $service     = 'cups-browsed',
@@ -17,11 +39,9 @@ define cups::config::rawtemplate (
     undef   => 'cups',
     default => 'cups-browsed',
   }
-  if ($file == '/etc/cups/cupsd.conf') {
-    $_chmod_value = '0640'
-  }
-  else {
-    $_chmod_value = $chmod_value
+  $_chmod_value = $file ? {
+    '/etc/cups/cupsd.conf' => '0640',
+    default                => $chmod_value,
   }
   if ($::osfamily == 'RedHat' and $::operatingsystemmajrelease == 6 and $file == '/etc/cups/cupsd.conf') {
     $include_file = [ 'Include /etc/cups/cups-browsed.conf' ]
@@ -32,7 +52,7 @@ define cups::config::rawtemplate (
   }
 
   file { $file:
-    ensure  => file,
+    ensure  => $ensure,
     content => template('cups/cups-browsed.erb'),
     mode    => $_chmod_value,
     notify  => Service[$service],
